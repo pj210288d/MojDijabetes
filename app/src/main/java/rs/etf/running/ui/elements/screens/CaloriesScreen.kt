d0f01b72fe47c1170214796aad92d8a9313e2024
Marko Micovic <marko.micovic.93@gmail.com>
2022-10-20 14:01:29 +0200

    Replace Greeting with OutlinedTextField for weight input

--- app/src/main/java/rs/etf/running/ui/elements/screens/CaloriesScreen.kt
+++ app/src/main/java/rs/etf/running/ui/elements/screens/CaloriesScreen.kt
@@ -1,15 +1,16 @@
 package rs.etf.running.ui.elements.screens
 
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun CaloriesScreen(modifier: Modifier = Modifier) {
-    Greeting("Android")
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    // https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary
+    OutlinedTextField(
+        label = { Text(text = "Tezina") },
+        value = "",
+        onValueChange = {},
+    )
 }
