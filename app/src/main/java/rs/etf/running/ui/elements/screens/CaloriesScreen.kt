d377bc6a795964de2d19347523538a662d2321f7
Marko Micovic <marko.micovic.93@gmail.com>
2022-10-20 14:27:45 +0200

    Set keyboard type for OutlinedTextField

--- app/src/main/java/rs/etf/running/ui/elements/screens/CaloriesScreen.kt
+++ app/src/main/java/rs/etf/running/ui/elements/screens/CaloriesScreen.kt
@@ -1,28 +1,27 @@
 package rs.etf.running.ui.elements.screens
 
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import rs.etf.running.LOG_TAG
 
 @Composable
 fun CaloriesScreen(modifier: Modifier = Modifier) {
-    Log.d(LOG_TAG, "(re)composition")
-
-    // https://developer.android.com/jetpack/compose/state
     var weight by remember { mutableStateOf("") }
 
-    // https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary
     OutlinedTextField(
         label = { Text(text = "Tezina") },
         value = weight,
         onValueChange = { newWeightValue -> weight = newWeightValue },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+        ),
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
