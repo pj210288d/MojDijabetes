64c4bb0ffa0a3788bfc47145a39eb44ebb24adf6
Marko Micovic <marko.micovic.93@gmail.com>
2022-10-20 16:38:08 +0200

    Add Scaffold with TopAppBar

--- app/src/main/java/rs/etf/running/ui/elements/RunningApp.kt
+++ app/src/main/java/rs/etf/running/ui/elements/RunningApp.kt
@@ -1,20 +1,27 @@
 package rs.etf.running.ui.elements
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import rs.etf.running.ui.elements.screens.CaloriesScreen
+import rs.etf.running.R
 
 @Composable
 fun RunningApp() {
     // A surface container using the 'background' color from the theme
     // https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
     ) {
-        CaloriesScreen()
+        Scaffold(topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.calories_toolbar_title)) },
+            )
+        }) { padding ->
+            CaloriesScreen(modifier = Modifier.padding(padding))
+        }
     }
 }
