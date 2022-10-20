f9e239cbba4b69564c325b61817f04a7bbbb1bdd
Marko Micovic <marko.micovic.93@gmail.com>
2022-10-20 16:32:00 +0200

    Add SavedStateHandle to ViewModel in order for data to survive unexpected process deaths

--- app/src/main/java/rs/etf/running/ui/stateholders/CaloriesViewModel.kt
+++ app/src/main/java/rs/etf/running/ui/stateholders/CaloriesViewModel.kt
@@ -1,10 +1,14 @@
 package rs.etf.running.ui.stateholders
 
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.parcelize.Parcelize
 
-// https://kotlinlang.org/docs/data-classes.html
+// https://developer.android.com/kotlin/parcelize
+@Parcelize
 data class CaloriesUiState(
     val weight: String = "",
     val height: String = "",
@@ -15,43 +19,55 @@ data class CaloriesUiState(
     val metIndex: Int = 0,
     val caloriesNeeded: Double? = null,
     val caloriesBurned: Double? = null,
-)
+) : Parcelable
 
-// https://developer.android.com/topic/libraries/architecture/viewmodel
-class CaloriesViewModel : ViewModel() {
-    // https://developer.android.com/kotlin/flow
-    private val _uiState = MutableStateFlow(CaloriesUiState())
-    val uiState = _uiState
+const val UI_STATE_KEY = "uiState"
+
+// $ adb devices -l
+// $ adb -t <transport-id> shell am kill <package-name>
+
+// https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-savedstate
+class CaloriesViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private val _uiState1 = MutableStateFlow(CaloriesUiState())
+
+    // https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-savedstate#savedstate-stateflow
+    private val _uiState2 = savedStateHandle.getStateFlow(UI_STATE_KEY, CaloriesUiState())
+
+    val uiState = _uiState2
 
     fun setWeight(weight: String) {
-        _uiState.update { currentCaloriesUiState ->
-            // https://kotlinlang.org/docs/data-classes.html#copying
-            currentCaloriesUiState.copy(weight = weight)
-        }
+        savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(weight = weight)
+        _uiState1.update { currentCaloriesUiState -> currentCaloriesUiState.copy(weight = weight) }
     }
 
     fun setHeight(height: String) {
-        _uiState.update { it.copy(height = height) }
+        savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(height = height)
+        _uiState1.update { it.copy(height = height) }
     }
 
     fun setAge(age: String) {
-        _uiState.update { it.copy(age = age) }
+        savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(age = age)
+        _uiState1.update { it.copy(age = age) }
     }
 
     fun setIsMale() {
-        _uiState.update { it.copy(isMale = true, isFemale = false) }
+        savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(isMale = true, isFemale = false)
+        _uiState1.update { it.copy(isMale = true, isFemale = false) }
     }
 
     fun setIsFemale() {
-        _uiState.update { it.copy(isFemale = true, isMale = false) }
+        savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(isFemale = true, isMale = false)
+        _uiState1.update { it.copy(isFemale = true, isMale = false) }
     }
 
     fun setDuration(duration: String) {
-        _uiState.update { it.copy(duration = duration) }
+        savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(duration = duration)
+        _uiState1.update { it.copy(duration = duration) }
     }
 
     fun setMetIndex(metIndex: Int) {
-        _uiState.update { it.copy(metIndex = metIndex) }
+        savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(metIndex = metIndex)
+        _uiState1.update { it.copy(metIndex = metIndex) }
     }
 
     fun calculateCalories(): Boolean {
@@ -70,7 +86,11 @@ class CaloriesViewModel : ViewModel() {
                 655.1 + 9.6 * weight + 1.9 * height - 4.7 * age
             val caloriesBurned = duration * metValue * 3.5 * weight / 200
 
-            _uiState.update {
+            savedStateHandle[UI_STATE_KEY] = _uiState2.value.copy(
+                caloriesNeeded = caloriesNeeded,
+                caloriesBurned = caloriesBurned
+            )
+            _uiState1.update {
                 it.copy(
                     caloriesNeeded = caloriesNeeded,
                     caloriesBurned = caloriesBurned
