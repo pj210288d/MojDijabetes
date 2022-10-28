1dd7990821b9965825bb0a3804d7dba458e9af21
Marko Micovic <marko.micovic.93@gmail.com>
2022-10-28 08:34:18 +0200

    Add empty RouteBrowseScreen

---
+++ app/src/main/java/rs/etf/running/ui/elements/screens/RouteBrowseScreen.kt
@@ -0,0 +1,9 @@
+package rs.etf.running.ui.elements.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RouteBrowseScreen(modifier: Modifier = Modifier) {
+
+}
