3a9966129a57adccc777e4bd5fae6d34b6d2a22f
Marko Micovic <marko.micovic.93@gmail.com>
2022-12-12 11:47:32 +0100

    Add RunningDatabase with Workout table

---
+++ app/src/main/java/rs/etf/running/data/room/WorkoutDao.kt
@@ -0,0 +1,19 @@
+package rs.etf.running.data.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WorkoutDao {
+
+    @Insert
+    suspend fun insert(workout: Workout)
+
+    @Query("SELECT * FROM Workout")
+    suspend fun getAll(): List<Workout>
+
+    @Query("SELECT * FROM Workout")
+    fun getAllAsFlow(): Flow<List<Workout>>
+}
